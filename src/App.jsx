262af891e8c881af33e4522bdb8e14ccc04b91e4
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2024-01-12 11:40:59 +0530

    Navbar issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ScrambleVisualizer from "./components/ScrambleVisualizer";
 import Navbar from "./components/Navbar";
 import { isMobile, isTablet } from "mobile-device-detect";
 import MobileNavbar from "./components/MobileNavbar";
+
 function App() {
   const [isRunning, setIsRunning] = useState(false);
   const [currScramble, setCurrScramble] = useState("");
@@ -13,11 +14,10 @@ function App() {
 
   return (
     <div className="App row">
-      {console.log(isMobile)}
-      <div className="col col-lg-2 col-md-1 col-sm-12" style={{padding: 0 }}>
-   {   isMobile ?<MobileNavbar/>:<Navbar />}
+      <div className="col col-lg-2 col-md-1 col-12" style={{ padding: 0 }}>
+        { window.innerWidth <= 767 ? <MobileNavbar /> : <Navbar />}
       </div>
-      {console.log(isTablet)}
+
       <div className="col col-lg-8 col-md-8 col-12" style={{ padding: 0 }}>
         {/* Content of the middle column */}
         <div className="timerPlayGround" style={{ width: "100%" }}>
@@ -38,7 +38,8 @@ function App() {
           />
         </div>
       </div>
-      <div className="col col-lg-2 col-md-3 col-sm-12" style={{ padding: 0 }}></div>
+
+      <div className="col col-lg-2 col-md-3 col-12" style={{ padding: 0 }}></div>
 
       <ScrambleVisualizer currPuzzle={currPuzzle} currScramble={currScramble} />
     </div>
