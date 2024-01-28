661587940356f9196bb58a38d8f7a4cdf6a3bef7
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2024-01-28 15:28:15 +0530

    bug solved

--- src/components/ScrambleVisualizer/ScrambleVisualizer.jsx
+++ src/components/ScrambleVisualizer/ScrambleVisualizer.jsx
@@ -7,19 +7,15 @@ export default function ScrambleVisualizer({
   currScramble,
   visualDimension,
 }) {
-  const TwistyPlayer2 = new TwistyPlayer(
-    (
-      <twisty-player
-        style={{ width: "280px", height: "280px" }}
-        alg={currScramble}
-        puzzle={currPuzzle}
-        hint-facelets="none"
-        visualization={visualDimension}
-        back-view="top-right"
-        control-panel="none"
-        background="none"
-      ></twisty-player>
-    )
-  );
-  return <div className="scrambleContainer">{TwistyPlayer2}</div>;
+
+  return <div className="scrambleContainer">      <twisty-player
+  style={{ width: "280px", height: "280px" }}
+  alg={currScramble}
+  puzzle={currPuzzle}
+  hint-facelets="none"
+  visualization={visualDimension}
+  back-view="top-right"
+  control-panel="none"
+  background="none"
+></twisty-player></div>;
 }
